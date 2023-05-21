11e0a5a2e1bb7a9abe716960ca75e383c75ebc41
EvaldasZak <evaldas.zaksas@gmail.com>
2023-05-21 18:31:32 +0300

    prettier

--- src/components/atoms/EditedTag.jsx
+++ src/components/atoms/EditedTag.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
+import React from "react";
 
-const EditedTag = ({edited}) => {
-        return <>{edited && <i>This question was edited.</i>}</>;
-}
+const EditedTag = ({ edited }) => {
+  return <>{edited && <i>This question was edited.</i>}</>;
+};
 
-export default EditedTag
+export default EditedTag;
